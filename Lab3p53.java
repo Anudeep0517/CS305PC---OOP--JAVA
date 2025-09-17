11290da66341066b2c5cd9932a95fa2859ea729d
Anudeep0517 <komatianudeep@gmail.com>
2025-09-17 12:51:19 +0530

    Add files via upload

---
+++ Lab3p53.java
@@ -0,0 +1,9 @@
+class Lab3p53{
+	public static void main(String...args){
+	 Lab3p53 p = new Lab3p53();
+	 
+	 p.setName("ACE ENGG COLLEGE");
+	 System.out.println(p.getName());
+	 p.demonstrate
+	}
+}
